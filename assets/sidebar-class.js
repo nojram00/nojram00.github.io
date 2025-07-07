56413c4602676590c4f402a8d50d63933ab3db6e
marjonnmi <marjon.godito@gmanmi.com>
2025-07-07 15:09:00 +0800

    reset

--- assets/sidebar-class.js
+++ assets/sidebar-class.js
@@ -1,37 +1,6 @@
 class HTMLSidebarElement extends HTMLElement {
     constructor(){
         super();
-
-        this.shadow = this.attachShadow({mode : 'open'});
-        this.style = document.createElement('style');
-    }
-
-    connectedCallback(){
-        this.style.textContent = `
-            :host {
-                width: 240px;
-                background-color: white;
-                position: fixed;
-                top: 0;
-                left: -240px;
-                transition: all 1s;
-                box-shadow: 0 0 10px rgba(0,0,0,0.2);
-                display: block;
-                height: 100%;
-            }
-
-            :host[open] {
-                left: 0px;
-            }
-
-            :host[open][partial] {
-                left: -75%;
-            }
-        `
-
-        this.shadow.append(style);
-
-        this.shadow.append(document.createElement('slot'));
     }
 
     open() {
