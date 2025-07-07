f8f5bd54ffdf826148fa3ca7b2f5e611bec3c895
marjonnmi <marjon.godito@gmanmi.com>
2025-07-07 09:43:40 +0800

    update sidebar script

--- assets/sidebar.js
+++ assets/sidebar.js
@@ -4,28 +4,33 @@ document.addEventListener('DOMContentLoaded', function(){
 })
 
 function loadSidebar(){
+
+    function openSideBar(sidebar){
+        sidebar.setAttribute('open', '');
+        document.querySelectorAll('[toggle-open]').forEach(e => e.setAttribute('disabled', '')); 
+        document.querySelectorAll('[toggle-close]').forEach(e => e.removeAttribute('disabled'));
+    }
+
+    function closeSideBar(sidebar){
+        sidebar.removeAttribute('open');
+        document.querySelectorAll('[toggle-open]').forEach(e => e.removeAttribute('disabled'));
+        document.querySelectorAll('[toggle-close]').forEach(e => e.setAttribute('disabled', ''));
+    }
+
     const sidebar = document.querySelector('sidebar');
     document.querySelectorAll('[toggle]').forEach(e => {
         e.addEventListener('click', function(){
-            sidebar.hasAttribute('open') ? sidebar.removeAttribute('open') : sidebar.setAttribute('open', '')
+            sidebar.hasAttribute('open') ? closeSideBar(sidebar) : openSideBar(sidebar);
         })
     })
     document.querySelectorAll('[toggle-open]').forEach(e => {
         e.addEventListener('click', () => {
-            if(!sidebar.hasAttribute('open')) {
-                sidebar.setAttribute('open', '');
-                document.querySelectorAll('[toggle-open]').forEach(e => e.setAttribute('disabled', '')); 
-                document.querySelectorAll('[toggle-close]').forEach(e => e.removeAttribute('disabled'));
-            }
+            !sidebar.hasAttribute('open') && openSideBar(sidebar);
         });
     }); 
     document.querySelectorAll('[toggle-close]').forEach(e => {
         e.addEventListener('click', () => {
-            if(sidebar.hasAttribute('open')) {
-                sidebar.removeAttribute('open');
-                document.querySelectorAll('[toggle-open]').forEach(e => e.removeAttribute('disabled'));
-                document.querySelectorAll('[toggle-close]').forEach(e => e.setAttribute('disabled', '')); 
-            };
+            sidebar.hasAttribute('open') && closeSideBar(sidebar);
         })
     })
 }
